58bd2f4410cad7e5a7fbe186f6af04722f2b9349
Fernando Bevilacqua <dovyski@gmail.com>
2016-08-07 19:26:13 +0200

    Rough implementation of custom tile editing

--- flixel/system/debug/interaction/tools/Tile.hx
+++ flixel/system/debug/interaction/tools/Tile.hx
@@ -1,11 +1,15 @@
 package flixel.system.debug.interaction.tools;
 
+import flash.display.Bitmap;
 import flash.display.BitmapData;
+import flash.events.MouseEvent;
 import flash.ui.Keyboard;
 import flixel.FlxSprite;
 import flixel.group.FlxGroup;
 import flixel.math.FlxPoint;
+import flixel.system.debug.Window;
 import flixel.tile.FlxTilemap;
+import openfl.display.Sprite;
 
 @:bitmap("assets/images/debugger/buttons/tile.png") 
 class GraphicTileTool extends BitmapData {}
@@ -22,7 +26,7 @@ class Tile extends Tool
 	private var _tileHightligh:FlxSprite;
 	private var _tilemaps:FlxGroup;
 	private var _activeTilemap:FlxTilemap;
-	private var _activeTileType:Int;
+	private var _properties:TilePropertiesWindow;
 	
 	override public function init(Brain:Interaction):Tool 
 	{
@@ -33,6 +37,10 @@ class Tile extends Tool
 		setCursor(new GraphicTileTool(0, 0));
 		
 		_tilemaps = new FlxGroup();
+		_tileHightligh = new FlxSprite(); // TODO: replace this with a Sprite.
+		_properties = new TilePropertiesWindow(this);
+
+		Brain.getContainer().addChild(_properties);
 		
 		return this;
 	}
@@ -47,14 +55,11 @@ class Tile extends Tool
 		
 		_activeTilemap = cast _tilemaps.getFirstAlive();
 		
-		// TODO: get tile width/height from selected tilemap
-		_tileHightligh = new FlxSprite();
-		_tileHightligh.makeGraphic(8, 8, 0xffff0000);
-		_tileHightligh.width = 8;
-		_tileHightligh.height = 8;
+		_tileHightligh.width = _activeTilemap.width / _activeTilemap.widthInTiles;
+		_tileHightligh.height = _activeTilemap.height / _activeTilemap.heightInTiles;
+		_tileHightligh.makeGraphic(cast _tileHightligh.width, cast _tileHightligh.height, 0xffff0000);
 		
-		// TODO: get this based on user input
-		_activeTileType = 3;
+		_properties.refresh(_activeTilemap);
 	}
 	
 	override public function update():Void 
@@ -76,7 +81,7 @@ class Tile extends Tool
 		{
 			if (_activeTilemap != null)
 			{
-				var b :Bool = _activeTilemap.setTile(Std.int(brain.flixelPointer.x / _tileHightligh.width), Std.int(brain.flixelPointer.y / _tileHightligh.height), brain.keyPressed(Keyboard.DELETE) ? 0 : _activeTileType);
+				var b :Bool = _activeTilemap.setTile(Std.int(brain.flixelPointer.x / _tileHightligh.width), Std.int(brain.flixelPointer.y / _tileHightligh.height), brain.keyPressed(Keyboard.DELETE) ? 0 : _properties.getSelectedTileType());
 			}
 		}
 	}
@@ -121,5 +126,92 @@ class Tile extends Tool
 		}
 		
 		return target;
+	}
+}
+
+class TilePropertiesWindow extends Window
+{
+	private var _tileType:Int;
+	private var _tileTool:Tile;
+	private var _tileHightligh:Sprite;
+	private var _tilemap:FlxTilemap;
+	private var _tilemapGraphic:Sprite;
+	private var _tilemapBitmap:Bitmap;
+	private var _graphicTile:FlxPoint;
+	
+	private function initLayout():Void
+	{
+		_tilemapGraphic = new Sprite();
+		_tilemapBitmap = new Bitmap();
+		
+		_tilemapGraphic.addChild(_tilemapBitmap);
+		
+		_tilemapGraphic.y = 20;
+		_tilemapGraphic.scaleX = 2; // TODO: get values from Flixel
+		_tilemapGraphic.scaleY = 2; // TODO: get values from Flixel
+		
+		addChild(_tilemapGraphic);
+	}
+	
+	public function new(TileTool:Tile) 
+	{
+		super("Tilemap editor", new GraphicTileTool(0, 0), 200, 100);
+		_tileTool = TileTool;
+		
+		initLayout();
+		
+		_tileHightligh = new Sprite();
+		_tileHightligh.graphics.lineStyle(1, 0xff0000);
+		_tileHightligh.graphics.drawRect(0, 0, 16, 16);
+		_tileHightligh.width = 16;
+		_tileHightligh.height = 16;
+		_graphicTile = new FlxPoint();
+		
+		addChild(_tileHightligh);
+		
+		reposition(2, 150);
+		
+		_tilemapGraphic.addEventListener(MouseEvent.MOUSE_MOVE, handleMouseOverGraphic);
+		_tilemapGraphic.addEventListener(MouseEvent.MOUSE_UP, handleClickGraphic);
+	}
+	
+	public function refresh(Tilemap:FlxTilemap):Void
+	{
+		_tilemap = Tilemap;
+		
+		if (_tilemap != null)
+		{
+			_tilemapBitmap.bitmapData = _tilemap.frames.parent.bitmap;
+			resize(_tilemapBitmap.bitmapData.width * 2, _tilemapBitmap.bitmapData.height * 2 + _tilemapGraphic.y);
+		}
+	}
+	
+	public function getSelectedTileType():Int
+	{
+		return _tileType;
+	}
+
+	private function handleMouseOverGraphic(Event:MouseEvent):Void
+	{		
+		_graphicTile.x = Math.floor(Event.localX / 8) * 8;
+		_graphicTile.y = Math.floor(Event.localY / 8) * 8;
+		
+		_tileHightligh.x = _graphicTile.x * 2;
+		_tileHightligh.y = _graphicTile.y * 2;
+		
+		_tileHightligh.y += 20;
+	}
+	
+	private function handleClickGraphic(Event:MouseEvent):Void
+	{
+		var tilesPerRow:Int = Std.int(_tilemapBitmap.bitmapData.width / 8);
+		var row:Int = Std.int(_graphicTile.y / 8);
+		var column:Int = Std.int(_graphicTile.x / 8);
+		var index = row * tilesPerRow + column;
+		
+		FlxG.log.add("Click: " + _graphicTile.x + "," + _graphicTile.y + ", " + tilesPerRow);
+		FlxG.log.add("row: " + row + ", col:" + column + ", idx: " + index);
+		
+		_tileType = index;
 	}
 }
